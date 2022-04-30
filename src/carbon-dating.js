537fd7ee8b5b1d78c1815019361f41630ae1d508
irUdalova <irinabondarenko.mail@gmail.com>
2022-04-30 16:38:26 +0300

    feat: implement solutions for cats, dating, team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  if(typeof sampleActivity !== 'string' || !sampleActivity) {
+    return false;
+  }
+
+  const sample = Number(sampleActivity);
+  if (isNaN(sample) || sample > MODERN_ACTIVITY || sample <= 0) {
+    return false;
+  }
+
+  const ratio = MODERN_ACTIVITY / sample;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  result = Math.log(ratio) / k;
+  return Math.ceil(result);  
 }
 
 module.exports = {
